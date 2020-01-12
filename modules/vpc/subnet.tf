b77f3ace167aa135d0a14296e44e0b642294c9d5
masaki-uu <masaki.ueno.3214@gmail.com>
2020-01-12 14:06:12 +0900

    Add public route table

--- modules/vpc/subnet.tf
+++ modules/vpc/subnet.tf
@@ -4,3 +4,13 @@ resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.this.id
   cidr_block = cidrsubnet(var.vpc_cidr_block, 8, count.index)
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+}
+
--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,3 +1,7 @@
 resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr_block
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+}
