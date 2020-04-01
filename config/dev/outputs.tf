c67a37dcd6057261d2dd2a55a1cf75d80793e03f
ue-noma <masaki.ueno.3214@gmail.com>
2020-04-01 23:44:30 +0900

    Add Makefile

--- config/dev/outputs.tf
+++ config/dev/outputs.tf
@@ -1,3 +1,7 @@
 output "eks_configmap" {
   value = module.eks.eks_configmap
 }
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,3 +1,7 @@
 output "eks_configmap" {
   value = local.eks_configmap
 }
+
+output "cluster_name" {
+  value = aws_eks_cluster.this.id
+}
