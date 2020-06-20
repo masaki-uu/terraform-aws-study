4891ea39d78d7bb3a69ae6772b3d736d88527d79
ue-noma <masaki.ueno.3214@gmail.com>
2020-06-20 09:01:08 +0900

    Define private network acl

---
+++ modules/vpc/nacl.tf
@@ -0,0 +1,61 @@
+resource "aws_default_network_acl" "private" {
+  default_network_acl_id     = aws_vpc.this.default_network_acl_id
+  subnet_ids = aws_subnet.private[*].id
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "${local.name_prefix}-nacl-private"
+  }
+}
+
+resource "aws_network_acl_rule" "private" {
+  count = length(local.private_nacl_ingress_rules)
+
+  network_acl_id = aws_default_network_acl.private.id
+  rule_number    = (count.index + 1) * 10
+  egress         = false
+  protocol       = local.private_nacl_ingress_rules[count.index]["protocol"]
+  rule_action    = local.private_nacl_ingress_rules[count.index]["action"]
+  cidr_block     = local.private_nacl_ingress_rules[count.index]["cidr_block"]
+  from_port      = local.private_nacl_ingress_rules[count.index]["from_port"]
+  to_port        = local.private_nacl_ingress_rules[count.index]["to_port"]
+}
+
+resource "aws_network_acl" "public" {
+  vpc_id     = aws_vpc.this.id
+  subnet_ids = aws_subnet.public[*].id
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "${local.name_prefix}-nacl-public"
+  }
+}
+
+resource "aws_network_acl_rule" "public" {
+  count = length(local.public_nacl_ingress_rules)
+
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = (count.index + 1) * 10
+  egress         = false
+  protocol       = local.public_nacl_ingress_rules[count.index]["protocol"]
+  rule_action    = local.public_nacl_ingress_rules[count.index]["action"]
+  cidr_block     = local.public_nacl_ingress_rules[count.index]["cidr_block"]
+  from_port      = local.public_nacl_ingress_rules[count.index]["from_port"]
+  to_port        = local.public_nacl_ingress_rules[count.index]["to_port"]
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -12,6 +12,40 @@ variable "name_prefix" {
   description = "Name tag prefix."
 }
 
+variable "public_nacl_add_ingress_rules" {
+  description = "Public network ACL additional ingress rules."
+  type        = list(map(string))
+  default     = [
+    {
+      protocol   = "tcp"
+      action     = "allow"
+      cidr_block = "0.0.0.0/0"
+      from_port  = 443
+      to_port    = 443
+    },
+  ]
+}
+
 locals {
   name_prefix = var.name_prefix
+
+  base_nacl_ingress_rules = [
+    {
+      protocol   = -1
+      action     = "allow"
+      cidr_block = var.vpc_cidr_block
+      from_port  = 0
+      to_port    = 0
+    },
+    {
+      protocol   = "tcp"
+      action     = "allow"
+      cidr_block = "0.0.0.0/0"
+      from_port  = 1024
+      to_port    = 65535
+    },
+  ]
+
+  public_nacl_ingress_rules  = concat(var.public_nacl_add_ingress_rules, local.base_nacl_ingress_rules)
+  private_nacl_ingress_rules = local.base_nacl_ingress_rules
 }
